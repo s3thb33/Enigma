2007a87baf5c7abeaf931c6a8932deb49f0085a3
s3thb33 <70522903+s3thb33@users.noreply.github.com>
2020-10-25 16:04:01 -0400

    Add files via upload

---
+++ JavaEnigma/EnigmaInput.java
@@ -0,0 +1,31 @@
+// EnigmaInput.java
+/** 
+  * Using Java to prototype console based input from user for Enigma. 
+  * access modifiers and modularization to be dealth with later.
+**/
+
+//apparently we need this to retrieve user input
+import java.util.Scanner; 
+
+// first class definition
+public class EnigmaInput 
+{
+    // Single shared Scanner for key input
+    private static Scanner scanner = new Scanner(System.in);
+    // process execution
+        public static void main(String[] args)
+        {
+            // Prompt user for message.
+            System.out.print("Type your message for Enigma: ");
+
+            // Get message from user.
+            String message = scanner.nextLine();
+
+            // Display the input back to user.
+            System.out.println( "Input: " + message );
+
+        } // end main method
+}// end EnigmaInput class
+
+/* Can message object (arg?) with input String be accessed outside of EnigmaInput class?
+perhaps this we will learn soon...*/
---
+++ JavaEnigma/HelloEnigma.java
@@ -0,0 +1,6 @@
+public class HelloEnigma
+{
+	public static void main(String[] args) {
+		System.out.println("Welcome to Enigma!");
+	}
+}
